6a09ae5fdf926a20da3fb915826e20ea8e6eb7a2
Banzai322 <33576776+Banzai322@users.noreply.github.com>
2019-12-12 18:31:12 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ let date = new Date();
 let a = date.getHours();
 
 if ((a >= 0) && (a <= 8)) {
-    changeBackground.style.backgroundImage = "url(../img/bg_login-page.png)";
+    changeBackground.style.backgroundImage = "url(img/bg_login-page.png)";
 } else if ((a >= 8) && (a < 16)) {
-    changeBackground.style.backgroundImage = "url(../img/minimalism-wallpaper-1920x1080-004.jpg)";
+    changeBackground.style.backgroundImage = "url(img/minimalism-wallpaper-1920x1080-004.jpg)";
 } else if ((a >= 16) && (a <= 24)) {
-    changeBackground.style.backgroundImage = "url(../img/headerbg.jpg)";
+    changeBackground.style.backgroundImage = "url(img/headerbg.jpg)";
 }
